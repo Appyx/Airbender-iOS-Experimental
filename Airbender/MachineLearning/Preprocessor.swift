ae0acca92e1af9f6aaa15e817601a45c52686e20
Robert Gstöttner BSc <robert.gstoettner94@gmail.com>
2018-12-13 21:52:21 +0100

    processing things

--- Airbender/MachineLearning/Preprocessor.swift
+++ Airbender/MachineLearning/Preprocessor.swift
@@ -10,10 +10,10 @@ import Foundation
 import Surge
 
 class Preprocessor {
-
+    
     var raw: DataFrame
     var processed: SampleFrame
-
+    
     init(frame: DataFrame) throws {
         self.raw = frame
         if raw.checkSampleLengths() == false {
@@ -21,21 +21,21 @@ class Preprocessor {
         }
         self.processed = raw.accX.featurelessCopy()
     }
-
-    func resample(toSize: Int) throws {
+    
+    func applyResampling(toSize: Int) throws {
         func resample(data: [Double], toSize: Int) -> [Double] {
             let sampleFactor = Double(data.count) / Double(toSize)
             var fraction = 0.00
-
+            
             print("sample factor: \(sampleFactor)")
             print("original data size: \(data.count)")
-
+            
             var sampledData = [Double]()
             var i = 0
             while i < data.count {
                 let step = Int(floor(sampleFactor + fraction))
                 fraction = (sampleFactor + fraction).truncatingRemainder(dividingBy: 1) + 0.000001
-
+                
                 sampledData.append(data[i])
                 i += step
             }
@@ -46,37 +46,55 @@ class Preprocessor {
             throw DimensionError.dimensionsNotEqual
         }
     }
-
+    
     func dropEmptySamples() {
         raw.filter { $0.features.count == 0 }
     }
-
+    
     func generateSignalLengths() {
         processed.append(other: raw.accX) { [Double($0.count)] }
     }
-
-    func generateMean() {
-        let result=raw.applyWindow(size: 20) { mean($0) }
+    
+    func generateMean(windowSize: Int) {
+        let result = raw.applyWindow(size: windowSize) { mean($0) }
         processed.append(other: result.flatten())
     }
     
-    
-    func generateMax() {
-        let result=raw.applyWindow(size: 20) { max($0) }
+    func generateMax(windowSize: Int) {
+        let result = raw.applyWindow(size: windowSize) { max($0) }
         processed.append(other: result.flatten())
     }
     
-    func generateMin() {
-        let result=raw.applyWindow(size: 20) { min($0) }
+    func generateMin(windowSize: Int) {
+        let result = raw.applyWindow(size: windowSize) { min($0) }
         processed.append(other: result.flatten())
     }
-
-    func generateFFT() {
-        let frequency = raw.apply { fft($0) }
-        let filtered = frequency.apply { Array($0.dropLast(50)) }
+    
+    func generateZeroCrossingRate() {
+        
+    }
+    
+    func applyLowpass(cutoff: Int) {
+        let transformed = raw.apply { fft($0) }
+        let filtered = transformed.apply {
+            let step = 50 / $0.count
+            let validRange = cutoff / step
+            let toDrop = $0.count - validRange
+            return Array($0.dropLast(toDrop)) }
+        filtered.applyInPlace{fft($0)}
+        raw=filtered
+    }
+    
+    func generateFrequencies(cutoff: Int) {
+        let transformed = raw.apply { fft($0) }
+        let filtered = transformed.apply {
+            let step = 50 / $0.count
+            let validRange = cutoff / step
+            let toDrop = $0.count - validRange
+            return Array($0.dropLast(toDrop)) }
         processed.append(other: filtered.flatten())
     }
-
+    
     enum DimensionError: Error {
         case dimensionsNotEqual
         case lengthsNotEqual
